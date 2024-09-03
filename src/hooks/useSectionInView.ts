c7d33f4a89bed95f3fe75e6506e47fa5617ce0b5
Hans Kristian Reynaldi <z5348378@ad.unsw.edu.au>
2024-09-04 03:21:56 +0700

    for some reason when i reduce threshold it works

--- src/hooks/useSectionInView.ts
+++ src/hooks/useSectionInView.ts
@@ -2,12 +2,11 @@ import { useEffect } from "react";
 import { useInView } from "react-intersection-observer";
 import { useActiveContext } from "../contexts/ActiveContext";
 
-export const useSectionInView = (sectionName: string, threshold = 0.75) => {
+export const useSectionInView = (sectionName: string, threshold = 0.25) => {
   const { ref, inView } = useInView({
     threshold,
   });
   const { setActive, timeOfLastClick } = useActiveContext();
-  // console.log(sectionName);
   useEffect(() => {
     if (inView && Date.now() - timeOfLastClick > 1000) {
       setActive(sectionName);
