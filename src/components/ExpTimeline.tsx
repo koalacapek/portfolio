f2017ee6c3eae5cb29b3f2f5ad3e935117b89e64
Hans Kristian Reynaldi <z5348378@ad.unsw.edu.au>
2024-09-04 02:17:11 +0700

    rearrange timeline and add spotlight

--- src/components/ExpTimeline.tsx
+++ src/components/ExpTimeline.tsx
@@ -4,55 +4,51 @@ import { Timeline } from "./ui/Timeline";
 export function ExpTimeline() {
   const data = [
     {
-      title: "2024",
+      title: "2022",
       content: (
         <div>
           <p className="text-neutral-800 dark:text-neutral-200 text-xs md:text-base font-normal mb-4">
-            Trying to self learn more concepts that is not taught in uni.
+            Started my Computer Science journey at UNSW!
           </p>
           <div className="mb-8">
             <div className="flex gap-2 items-center text-neutral-700 dark:text-neutral-300 text-xs md:text-base">
-              ✅ Secured first internship as a
-              <span className="font-semibold">Frontend Developer</span> at
-              <span className="font-semibold">DAPPA</span>
+              ✅ Hello, World!
             </div>
             <div className="flex gap-2 items-center text-neutral-700 dark:text-neutral-300 text-xs md:text-base">
-              ✅ Joined another hackathon by UNSW CSESoc and DevSoc (did not
-              win🥲🥲)
+              ✅ Array, LinkedList, Stack, Queue
             </div>
             <div className="flex gap-2 items-center text-neutral-700 dark:text-neutral-300 text-xs md:text-base">
-              ✅ Got another (remote) internship! As a{" "}
-              <span className="font-semibold">Full Stack Engineer</span>
+              ✅ Tree, Graph, Heaps, Priority Queue🤯🤯
             </div>
             <div className="flex gap-2 items-center text-neutral-700 dark:text-neutral-300 text-xs md:text-base">
-              ✅ More to come!
+              ✅ JAVASCRIPT and TYPESCRIPT😍😍
             </div>
           </div>
           <div className="grid grid-cols-2 gap-4">
             <img
-              src="https://assets.aceternity.com/templates/startup-1.webp"
-              alt="startup template"
+              src="https://assets.aceternity.com/pro/hero-sections.png"
+              alt="hero template"
               width={500}
               height={500}
               className="rounded-lg object-cover h-20 md:h-44 lg:h-60 w-full shadow-[0_0_24px_rgba(34,_42,_53,_0.06),_0_1px_1px_rgba(0,_0,_0,_0.05),_0_0_0_1px_rgba(34,_42,_53,_0.04),_0_0_4px_rgba(34,_42,_53,_0.08),_0_16px_68px_rgba(47,_48,_55,_0.05),_0_1px_0_rgba(255,_255,_255,_0.1)_inset]"
             />
             <img
-              src="https://assets.aceternity.com/templates/startup-2.webp"
-              alt="startup template"
+              src="https://assets.aceternity.com/features-section.png"
+              alt="feature template"
               width={500}
               height={500}
               className="rounded-lg object-cover h-20 md:h-44 lg:h-60 w-full shadow-[0_0_24px_rgba(34,_42,_53,_0.06),_0_1px_1px_rgba(0,_0,_0,_0.05),_0_0_0_1px_rgba(34,_42,_53,_0.04),_0_0_4px_rgba(34,_42,_53,_0.08),_0_16px_68px_rgba(47,_48,_55,_0.05),_0_1px_0_rgba(255,_255,_255,_0.1)_inset]"
             />
             <img
-              src="https://assets.aceternity.com/templates/startup-3.webp"
-              alt="startup template"
+              src="https://assets.aceternity.com/pro/bento-grids.png"
+              alt="bento template"
               width={500}
               height={500}
               className="rounded-lg object-cover h-20 md:h-44 lg:h-60 w-full shadow-[0_0_24px_rgba(34,_42,_53,_0.06),_0_1px_1px_rgba(0,_0,_0,_0.05),_0_0_0_1px_rgba(34,_42,_53,_0.04),_0_0_4px_rgba(34,_42,_53,_0.08),_0_16px_68px_rgba(47,_48,_55,_0.05),_0_1px_0_rgba(255,_255,_255,_0.1)_inset]"
             />
             <img
-              src="https://assets.aceternity.com/templates/startup-4.webp"
-              alt="startup template"
+              src="https://assets.aceternity.com/cards.png"
+              alt="cards template"
               width={500}
               height={500}
               className="rounded-lg object-cover h-20 md:h-44 lg:h-60 w-full shadow-[0_0_24px_rgba(34,_42,_53,_0.06),_0_1px_1px_rgba(0,_0,_0,_0.05),_0_0_0_1px_rgba(34,_42,_53,_0.04),_0_0_4px_rgba(34,_42,_53,_0.08),_0_16px_68px_rgba(47,_48,_55,_0.05),_0_1px_0_rgba(255,_255,_255,_0.1)_inset]"
@@ -117,52 +113,57 @@ export function ExpTimeline() {
         </div>
       ),
     },
+
     {
-      title: "2022",
+      title: "2024",
       content: (
         <div>
           <p className="text-neutral-800 dark:text-neutral-200 text-xs md:text-base font-normal mb-4">
-            Started my Computer Science journey at UNSW!
+            Trying to self learn more concepts that is not taught in uni.
           </p>
           <div className="mb-8">
             <div className="flex gap-2 items-center text-neutral-700 dark:text-neutral-300 text-xs md:text-base">
-              ✅ Hello, World!
+              ✅ Secured first internship as a
+              <span className="font-semibold">Frontend Developer</span> at
+              <span className="font-semibold">DAPPA</span>
             </div>
             <div className="flex gap-2 items-center text-neutral-700 dark:text-neutral-300 text-xs md:text-base">
-              ✅ Array, LinkedList, Stack, Queue
+              ✅ Joined another hackathon by UNSW CSESoc and DevSoc (did not
+              win🥲🥲)
             </div>
             <div className="flex gap-2 items-center text-neutral-700 dark:text-neutral-300 text-xs md:text-base">
-              ✅ Tree, Graph, Heaps, Priority Queue🤯🤯
+              ✅ Got another (remote) internship! As a{" "}
+              <span className="font-semibold">Full Stack Engineer</span>
             </div>
             <div className="flex gap-2 items-center text-neutral-700 dark:text-neutral-300 text-xs md:text-base">
-              ✅ JAVASCRIPT and TYPESCRIPT😍😍
+              ✅ More to come!
             </div>
           </div>
           <div className="grid grid-cols-2 gap-4">
             <img
-              src="https://assets.aceternity.com/pro/hero-sections.png"
-              alt="hero template"
+              src="https://assets.aceternity.com/templates/startup-1.webp"
+              alt="startup template"
               width={500}
               height={500}
               className="rounded-lg object-cover h-20 md:h-44 lg:h-60 w-full shadow-[0_0_24px_rgba(34,_42,_53,_0.06),_0_1px_1px_rgba(0,_0,_0,_0.05),_0_0_0_1px_rgba(34,_42,_53,_0.04),_0_0_4px_rgba(34,_42,_53,_0.08),_0_16px_68px_rgba(47,_48,_55,_0.05),_0_1px_0_rgba(255,_255,_255,_0.1)_inset]"
             />
             <img
-              src="https://assets.aceternity.com/features-section.png"
-              alt="feature template"
+              src="https://assets.aceternity.com/templates/startup-2.webp"
+              alt="startup template"
               width={500}
               height={500}
               className="rounded-lg object-cover h-20 md:h-44 lg:h-60 w-full shadow-[0_0_24px_rgba(34,_42,_53,_0.06),_0_1px_1px_rgba(0,_0,_0,_0.05),_0_0_0_1px_rgba(34,_42,_53,_0.04),_0_0_4px_rgba(34,_42,_53,_0.08),_0_16px_68px_rgba(47,_48,_55,_0.05),_0_1px_0_rgba(255,_255,_255,_0.1)_inset]"
             />
             <img
-              src="https://assets.aceternity.com/pro/bento-grids.png"
-              alt="bento template"
+              src="https://assets.aceternity.com/templates/startup-3.webp"
+              alt="startup template"
               width={500}
               height={500}
               className="rounded-lg object-cover h-20 md:h-44 lg:h-60 w-full shadow-[0_0_24px_rgba(34,_42,_53,_0.06),_0_1px_1px_rgba(0,_0,_0,_0.05),_0_0_0_1px_rgba(34,_42,_53,_0.04),_0_0_4px_rgba(34,_42,_53,_0.08),_0_16px_68px_rgba(47,_48,_55,_0.05),_0_1px_0_rgba(255,_255,_255,_0.1)_inset]"
             />
             <img
-              src="https://assets.aceternity.com/cards.png"
-              alt="cards template"
+              src="https://assets.aceternity.com/templates/startup-4.webp"
+              alt="startup template"
               width={500}
               height={500}
               className="rounded-lg object-cover h-20 md:h-44 lg:h-60 w-full shadow-[0_0_24px_rgba(34,_42,_53,_0.06),_0_1px_1px_rgba(0,_0,_0,_0.05),_0_0_0_1px_rgba(34,_42,_53,_0.04),_0_0_4px_rgba(34,_42,_53,_0.08),_0_16px_68px_rgba(47,_48,_55,_0.05),_0_1px_0_rgba(255,_255,_255,_0.1)_inset]"
