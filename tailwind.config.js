d9e11a3fc5d7e5dd6ac518db1787259d09509499
Hans Kristian Reynaldi <z5348378@ad.unsw.edu.au>
2024-09-04 02:28:12 +0700

    add animation

--- tailwind.config.js
+++ tailwind.config.js
@@ -18,6 +18,21 @@ module.exports = {
       scale: {
         102: "1.02",
       },
+      animation: {
+        spotlight: "spotlight 2s ease .75s 1 forwards",
+      },
+      keyframes: {
+        spotlight: {
+          "0%": {
+            opacity: 0,
+            transform: "translate(-72%, -62%) scale(0.5)",
+          },
+          "100%": {
+            opacity: 1,
+            transform: "translate(-50%,-40%) scale(1)",
+          },
+        },
+      },
     },
   },
   plugins: [
