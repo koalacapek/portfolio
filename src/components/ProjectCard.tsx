f8fe512e7c62b34f7ed6f84b5b3ba74eae1f5377
Hans Kristian Reynaldi <z5348378@ad.unsw.edu.au>
2024-09-04 18:03:42 +0700

    renive unusused code

--- src/components/ProjectCard.tsx
+++ src/components/ProjectCard.tsx
@@ -23,20 +23,6 @@ const ProjectCard = (props: CardProps) => {
         </div>
       </PinContainer>
     </div>
-    // <div
-    //   onClick={handleButtonClick}
-    //   className="rounded-lg shadow-lg overflow-hidden w-[45%] h-80 flex flex-col gap-4  bg-deepblue hover:shadow-md hover:scale-105 cursor-pointer transition-transform duration-300 ease-in-out"
-    // >
-    //   <img
-    //     className="w-full h-[55%] object-cover"
-    //     src={props.img}
-    //     alt={props.name}
-    //   />
-    //   <div className="pl-4">
-    //     <h3 className="text-lg font-semibold text-white mb-2">{props.name}</h3>
-    //     <p className="text-white text-sm">{props.desc}</p>
-    //   </div>
-    // </div>
   );
 };
 
