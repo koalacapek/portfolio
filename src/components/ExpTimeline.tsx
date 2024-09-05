324911d527392c51b2fa184b8a67d075a1c12081
koalacapek <hanskr7882@gmail.com>
2024-09-06 00:27:10 +0700

    remomve unused cofe

--- src/components/ExpTimeline.tsx
+++ src/components/ExpTimeline.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import { Timeline } from "./ui/Timeline";
 
 import comp1511 from "../assets/comp1511.jpg";
@@ -179,13 +178,6 @@ export function ExpTimeline() {
               height={500}
               className="rounded-lg object-cover h-20 md:h-44 lg:h-60 w-full shadow-[0_0_24px_rgba(34,_42,_53,_0.06),_0_1px_1px_rgba(0,_0,_0,_0.05),_0_0_0_1px_rgba(34,_42,_53,_0.04),_0_0_4px_rgba(34,_42,_53,_0.08),_0_16px_68px_rgba(47,_48,_55,_0.05),_0_1px_0_rgba(255,_255,_255,_0.1)_inset]"
             />
-            {/* <img
-              src="https://assets.aceternity.com/templates/startup-4.webp"
-              alt="startup template"
-              width={500}
-              height={500}
-              className="rounded-lg object-cover h-20 md:h-44 lg:h-60 w-full shadow-[0_0_24px_rgba(34,_42,_53,_0.06),_0_1px_1px_rgba(0,_0,_0,_0.05),_0_0_0_1px_rgba(34,_42,_53,_0.04),_0_0_4px_rgba(34,_42,_53,_0.08),_0_16px_68px_rgba(47,_48,_55,_0.05),_0_1px_0_rgba(255,_255,_255,_0.1)_inset]"
-            /> */}
           </div>
         </div>
       ),
