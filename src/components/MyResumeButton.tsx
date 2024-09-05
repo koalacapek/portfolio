dad63fcd55fd2c64c3d61f3a7158eea11d43de9d
koalacapek <hanskr7882@gmail.com>
2024-09-06 01:13:24 +0700

    remove more unused code

--- src/components/MyResumeButton.tsx
+++ src/components/MyResumeButton.tsx
@@ -1,9 +1,4 @@
 const MyResumeButton = () => {
-  // const handleOpenResume = () => {
-  //   const pdfPath = process.env.PUBLIC_URL + "/resume.pdf";
-  //   window.open(pdfPath, "_blank");
-  // };
-
   return (
     <a
       className="hover:bg-white hover:text-black hover:border-black py-4 border-white border px-8 transition duration-300 ease-in-out text-xl rounded-xl"
