bd72399b35a15eda0233f1a6789ec4d1db37b81a
koalacapek <hanskr7882@gmail.com>
2024-09-06 14:42:35 +0700

    done

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -1,10 +1,13 @@
 import MyResumeButton from "../components/MyResumeButton";
 import ScrollAnimation from "../components/ScrollAnimation";
+import { Spotlight } from "../components/ui/Spotlight";
+
 import { useSectionInView } from "../hooks/useSectionInView";
+
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faGithub, faLinkedin } from "@fortawesome/free-brands-svg-icons";
+
 import { Fade } from "react-awesome-reveal";
-import { Spotlight } from "../components/ui/Spotlight";
 
 const Hero = () => {
   const { ref } = useSectionInView("Home");
